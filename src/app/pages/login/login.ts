ffbe28a48a2509d1a5f03b3079a5403287791311
julia-kocik <julia.kocik@gmail.com>
2026-07-25 12:43:55 +0200

    Add login page

---
+++ src/app/pages/login/login.ts
@@ -0,0 +1,41 @@
+import { Component } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  imports: [ReactiveFormsModule],
+  templateUrl: './login.html',
+  styleUrl: './login.scss',
+})
+export class Login {
+  readonly loginForm = new FormGroup({
+    email: new FormControl('', {
+      nonNullable: true,
+      validators: [Validators.required, Validators.email],
+    }),
+    password: new FormControl('', {
+      nonNullable: true,
+      validators: [
+        Validators.required,
+        Validators.minLength(6),
+      ],
+    }),
+    rememberMe: new FormControl(false, {
+      nonNullable: true,
+    }),
+  });
+
+  onSubmit(): void {
+    if (this.loginForm.invalid) {
+      this.loginForm.markAllAsTouched();
+      return;
+    }
+
+    console.log(this.loginForm.getRawValue());
+  }
+}

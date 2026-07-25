33f67d31ec95be1f22d66d2b5629fbdfcd4a3d54
julia-kocik <julia.kocik@gmail.com>
2026-07-25 12:43:25 +0200

    Add section wrapper

---
+++ src/app/components/common/section-wrapper/section-wrapper.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-section-wrapper',
+  imports: [],
+  templateUrl: './section-wrapper.html',
+  styleUrl: './section-wrapper.scss',
+})
+export class SectionWrapper {}

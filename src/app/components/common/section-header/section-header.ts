f7d2bf9760ac43d0d89146dfb161e2f5018ee9e9
julia-kocik <julia.kocik@gmail.com>
2026-07-25 12:42:35 +0200

    Add section header

---
+++ src/app/components/common/section-header/section-header.ts
@@ -0,0 +1,11 @@
+import { Component, input } from '@angular/core';
+
+@Component({
+  selector: 'app-section-header',
+  imports: [],
+  templateUrl: './section-header.html',
+  styleUrl: './section-header.scss',
+})
+export class SectionHeader {
+  readonly text = input.required<string>();
+}
